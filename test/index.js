3101784c2d9eec170fc47364001b546289efb662
sethlu <luzhuo970629@gmail.com>
2016-03-04 17:41:03 +0800

    Use Travis CI
    Trigger Travis CI
    
    Do not test with Node 0.6 or 0.8

--- test/index.js
+++ test/index.js
@@ -1,5 +1,4 @@
-// var child = require('child_process')
-// var path = require('path')
+var child = require('child_process')
 
 var series = require('run-series')
 
@@ -7,10 +6,22 @@ var util = require('./util')
 
 series([
   function (cb) {
+    child.exec('which codesign', cb)
+  },
+  function (stdout, stderr, cb) {
+    if (stderr) {
+      return cb(new Error('Unable to perform tests without codesign.'))
+    }
     console.log('Calling electron-download before running tests...')
     util.downloadElectrons(cb)
   }
-], function () {
+], function (err) {
+  if (err) {
+    console.error('Test failed.')
+    if (err.message) console.error(err.message)
+    else console.error(err, err.stack)
+    return
+  }
   console.log('Running tests...')
   if (process.platform !== 'darwin') {
     console.error('Unable to perform tests on non-darwin platforms.')
