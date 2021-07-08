961590da9169c90bc07c86dfcf668dc236bd2b4a
Florian Imdahl <git@ffflorian.de>
2021-07-08 14:31:12 +0200

    fix: Update TypeScript types

--- index.d.ts
+++ index.d.ts
@@ -2,25 +2,29 @@ declare module "electron-osx-sign" {
   interface BaseSignOptions {
     app: string;
     identity?: string;
-    platform?: string;
+    'identity-validation'?: boolean;
     keychain?: string;
+    platform?: string;
   }
 
   interface SignOptions extends BaseSignOptions {
     binaries?: string[];
     entitlements?: string;
+    entitlementsForFile?: (filePath: string, args: string[]) => void;
     'entitlements-inherit'?: string;
     'entitlements-loginhelper'?: string;
     'gatekeeper-assess'?: boolean;
     hardenedRuntime?: boolean;
-    'identity-validation'?: boolean;
     ignore?: string;
     'pre-auto-entitlements'?: boolean;
     'pre-embed-provisioning-profile'?: boolean;
     'provisioning-profile'?: string;
     'requirements'?: string;
     'signature-size'?: number;
-    'type'?: string;
+    'signature-flags'?: string | string[];
+    'strict-verify'?: boolean | string | string[];
+    timestamp?: string;
+    type?: string;
     version?: string;
   }
 
@@ -29,7 +33,6 @@ declare module "electron-osx-sign" {
   export function signAsync(opts: SignOptions): Promise<any>;
 
   interface FlatOptions extends BaseSignOptions {
-    'identity-validation'?: boolean;
     install?: string;
     pkg?: string;
     scripts?: string;
