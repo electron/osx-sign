e05e008742da43a080d143112f941f73320171d0
Clifton Hensley <chensley@slack-corp.com>
2024-04-21 21:18:45 -0700

    feat: export walkAsync, correcting type definition comments around requirements (#311)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { sign, signApp } from './sign';
 import { flat, buildPkg } from './flat';
+import { walkAsync } from './util';
 
 // TODO: Remove and leave only proper named exports, but for non-breaking change reasons
 // we need to keep this weirdness for now
@@ -10,6 +11,7 @@ module.exports.signApp = signApp;
 module.exports.flat = flat;
 module.exports.flatAsync = buildPkg;
 module.exports.buildPkg = buildPkg;
+module.exports.walkAsync = walkAsync;
 
-export { sign, flat, signApp as signAsync, signApp, buildPkg as flatAsync, buildPkg };
+export { sign, flat, signApp as signAsync, signApp, buildPkg as flatAsync, buildPkg, walkAsync };
 export * from './types';
